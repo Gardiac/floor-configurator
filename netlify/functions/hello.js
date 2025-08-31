e4998b2660c2049cf9071ae11d458b0826ef92bd
Gardiac <benjamin.dzele@gmail.com>
2025-08-31 02:41:37 +0200

    Add files via upload

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,8 +1,5 @@
 export const handler = async () => ({
   statusCode: 200,
-  headers: {
-    "Access-Control-Allow-Origin": "*",
-    "Content-Type": "application/json"
-  },
+  headers: { "Access-Control-Allow-Origin": "*", "Content-Type": "application/json" },
   body: JSON.stringify({ ok: true, msg: "hello from Netlify functions" })
 });
